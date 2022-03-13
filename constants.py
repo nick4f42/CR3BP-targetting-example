889cf436da5c8328eeac1d13fe7a80be9a77e161
Nick OBrien <nick4f42@gmail.com>
2022-03-13 15:43:31 -0500

    Document `constants.py`

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
+# distance from earth to moon
 l_star = 3.850e5  # km
+
+# inverse of mean motion
 t_star = 3.758e5  # s
-mu = 1.21505856e-2  # nd
+
 mu_earth = 3.986e5  # km^3 / s^2
 mu_moon = 4902.8  # km^3 / s^2
+mu = mu_moon / (mu_earth + mu_moon)
